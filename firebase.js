dad59d6c629b8456dc709883a1698f08a6ca0325
Alex Fresneda <afres97@gmail.com>
2023-05-05 20:46:56 +0200

    install next-auth and firebase and initialise the firebase

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "memories-v1.firebaseapp.com",
+  projectId: "memories-v1",
+  storageBucket: "memories-v1.appspot.com",
+  messagingSenderId: "408161153390",
+  appId: "1:408161153390:web:72a1985d8990d723c7917f",
+  measurementId: "G-1DRN377TBX",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+export { app, db, storage };
+const analytics = getAnalytics(app);
