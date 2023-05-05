1fe19a09680a529f8d37bb6fd91bba109eb9c283
Alex Fresneda <afres97@gmail.com>
2023-05-05 21:34:00 +0200

    complete signin page

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "memories-v1.firebaseapp.com",
   projectId: "memories-v1",
   storageBucket: "memories-v1.appspot.com",
---
+++ pages/auth/signin.js
@@ -0,0 +1,32 @@
+import { getProviders, signIn } from "next-auth/react";
+
+export default function signin({ providers }) {
+  return (
+    <div className="flex h-[100vh] flex-col items-center justify-center">
+      <img className="mb-8 w-24" src="/logo.png" alt="memories logo" />
+      <div className="">
+        {Object.values(providers).map((provider) => (
+          <div className="flex flex-col items-center">
+            <h1 className="mb-8 text-2xl font-bold">Welcome to memories</h1>
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="rounded-full bg-black px-5 py-2 text-white hover:bg-opacity-90 dark:bg-white dark:text-black
+            "
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
